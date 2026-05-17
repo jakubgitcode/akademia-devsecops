c930f08011d091f1e130e836d57ec39b0903ce57
Jakub <jmuszynski@bluesoft.com>
2026-05-17 20:20:27 +0200

    przyklad blok4 - fix4

--- przyklad_blok4/infra/terraform.tf
+++ przyklad_blok4/infra/terraform.tf
@@ -11,10 +11,11 @@ terraform {
   # Backend z partial configuration — key nadawany z CLI per env:
   #   terraform init -backend-config="key=blok4/dev.terraform.tfstate"
   backend "azurerm" {
-    resource_group_name  = "rg-terraform-state"
-    storage_account_name = "stterraformstate"
-    container_name       = "tfstate"
-    use_oidc             = true
+    storage_account_name = "tflab1sa30vv44"
+    container_name       = "mojstan"
+    key                  = "lab4-repo-ci.terraform.tfstate"
+    resource_group_name  = "tf-lab1-rg30vv44"
+    use_azuread_auth     = true
   }
 }
 
