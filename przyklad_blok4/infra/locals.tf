f2b1aa619662097aa6bbbc6252885ccb724eb3e7
Jakub <jmuszynski@bluesoft.com>
2026-05-18 11:37:08 +0200

    fix fmt przyklad_blok4/infra/locals.tf

--- przyklad_blok4/infra/locals.tf
+++ przyklad_blok4/infra/locals.tf
@@ -6,7 +6,7 @@ locals {
     Project     = var.project_name
     ManagedBy   = "terraform"
     Pipeline    = "github-actions"
-    Owner       =  "Kuba"
+    Owner       = "Kuba"
   })
 
   # Storage account name: max 24 chars, lowercase, no hyphens
